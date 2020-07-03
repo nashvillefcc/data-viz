67b57e852d700e5fd7578237e2f19cd94239017b
Alex <rollinglex@gmail.com>
2020-07-02 22:24:02 -0500

    reformat layout width

--- src/Components/Layout/LayoutStyled.js
+++ src/Components/Layout/LayoutStyled.js
@@ -2,6 +2,6 @@ import styled from 'styled-components'
 import { Container, Divider, Button } from 'semantic-ui-react'
 export const LayoutWrapper = styled(Container)`
   height: 100vh;
+  margin: 0 !important;
   display: flex !important;
-  width: 100% !important;
 `
